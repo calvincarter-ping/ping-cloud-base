e837e3e37ecb24db6581f263fc8741fbd8f2d857
Colin Reininger <colinreininger@pingidentity.com>
2024-05-28 10:57:54 -0600

    Add integration test for auth policy actions added

--- tests/pingone/prerequisites/test_p1_tenant_configuration.py
+++ tests/pingone/prerequisites/test_p1_tenant_configuration.py
@@ -19,6 +19,20 @@ class TestP1EnvSetupAndTeardown(p1_test_base.P1TestBase):
         p1_auth_policy = self.get(self.cluster_env_endpoints.sign_on_policies, auth_policy_name)
         self.assertTrue(p1_auth_policy, f"Authentication policy '{auth_policy_name}' not created")
 
+    def test_authentication_policy_actions_added(self):
+        auth_policy_name = f"client-{self.tenant_name}"
+        p1_auth_policy = self.get(self.cluster_env_endpoints.sign_on_policies, auth_policy_name)
+        res = self.worker_app_token_session.get(p1_auth_policy.get("_links").get("actions").get("href"))
+        p1_auth_policy_actions = res.json()["_embedded"]["actions"]
+        print(p1_auth_policy_actions)
+        actions = ["LOGIN", "MULTI_FACTOR_AUTHENTICATION"]
+        for action in actions:
+            with self.subTest(msg=f"{action} added"):
+                self.assertTrue(
+                    any(action in a.get("type") for a in p1_auth_policy_actions),
+                    f"Authentication policy '{auth_policy_name}' action '{action}' not added",
+                )
+
 
 if __name__ == "__main__":
     unittest.main()
