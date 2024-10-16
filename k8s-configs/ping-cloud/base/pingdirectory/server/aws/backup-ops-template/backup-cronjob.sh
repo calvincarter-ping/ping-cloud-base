485f0a503ceb290bd8225cfa858e44a351182f6b
calvincarter <calvincarter@pingidentity.com>
2024-10-16 16:21:17 -0500

    [skip_pipeline] Refactor backups by making reusable methods and new label pd-manual

--- k8s-configs/ping-cloud/base/pingdirectory/server/aws/backup-ops-template/backup-cronjob.sh
+++ k8s-configs/ping-cloud/base/pingdirectory/server/aws/backup-ops-template/backup-cronjob.sh
@@ -2,15 +2,11 @@
 
 # Variables
 BACKUP_NAME="pingdirectory-backup"
-NAMESPACE="ping-cloud"
+CRON_JOB_BACKUP_NAME="pingdirectory-periodic-backup"
 SCRIPT="/opt/in/backup-ops.sh"
 SKIP_RESOURCE_CLEANUP="false"
 
 # Functions
-# Function to get the full pod name based on the prefix name 'pingdirectory-backup'
-get_pod_name() {
-  kubectl get pods -n "${NAMESPACE}" --no-headers -o custom-columns=":metadata.name" | grep "^${BACKUP_NAME}" | head -n 1
-}
 
 # Function to check determine when a Job is complete
 is_job_complete() {
@@ -24,7 +20,26 @@ get_failed_attempts() {
 
 # Function to get the number of backofflimits/# of retries for Job
 get_backoff_limit() {
-  kubectl get job "${BACKUP_NAME}" -n "${NAMESPACE}" -o jsonpath='{.spec.backoffLimit}'
+  kubectl get job "${BACKUP_NAME}" -n "${NAMESPACE}" -o jsonpath='{.spec.backoffLimit}' 2>/dev/null
+}
+
+# Function to get actively running Jobs, sorted by the time they were created.
+# This will filter out previous Jobs that are retained in the cluster that have failed.
+# This is done by jq which filters out status of active and ready.
+get_actively_running_manual_jobs() {
+  kubectl get jobs -l pd-manual=true -o json -n "${NAMESPACE}" --sort-by=.metadata.creationTimestamp 2>/dev/null | jq -r '.items[] | select(.status.active == 1 and .status.ready == 1) | .metadata.name'
+}
+
+# Function to get actively running CronJobs, sorted by the time they were created.
+# This will filter out previous CronJobs that are retained in the cluster that have failed.
+# This is done by jq which filters out status of active and ready.
+get_actively_running_cronjob() {
+  kubectl get jobs -o json -n "${NAMESPACE}" --sort-by=.metadata.creationTimestamp 2>/dev/null | jq -r '.items[] | select(.status.active == 1 and .status.ready == 1 and (.metadata.name | contains("pingdirectory-periodic-backup"))) | .metadata.name'
+}
+
+# Function to determine if a cronjob is actively running.
+is_cronjob_running_now() {
+  kubectl get cronjob "${CRON_JOB_BACKUP_NAME}" -n "${NAMESPACE}" -o jsonpath='{.status.active}' 2>/dev/null
 }
 
 # Function to delete Job and its PVC if detected in cluster
@@ -46,20 +61,19 @@ cleanup_resources() {
   fi
 }
 
-check_if_another_backup_is_running() {
-    # Determine if Cronjob is Active.
-    num_of_active_cronjobs=$(kubectl get cronjob pingdirectory-periodic-backup -n "${NAMESPACE}" -o jsonpath='{.status.active}' 2>/dev/null)
-
-    if [ -n "${num_of_active_cronjobs}" ]; then
-
+# Function ensures that only one backup Job will run. This will stop CronJob and Jobs from clashing and using
+# the same shared PVC.
+is_only_one_backup_running() {
+    # Determine if Cronjob is actively running. This will be empty if CronJob is not running.
+    active_cronjob_running=$(is_cronjob_running_now)
+    if [ -n "${active_cronjob_running}" ]; then
         # Entering this condition means there is an active Cronjob running.
 
         # Determine if Manual Job is also running while Cronjob is running.
-        # Manual jobs can be found by filtering on "manual=true" label.
-        active_manual_job_name=$(kubectl get jobs --selector=manual=true -o jsonpath='{.items[0].metadata.name}' -n "${NAMESPACE}" 2>/dev/null)
-
+        # Manual jobs can be found by filtering on "pd-manual=true" label.
+        active_manual_job_name=$(get_actively_running_manual_jobs | head -n 1)
         if [ -z "${active_manual_job_name}" ]; then
-            echo "Exiting because a manual Job was not found. There is not collision with Cronjob and manual Job."
+            echo "Manual Job was not found. There is not collision with Cronjob and manual Job. Ready to proceed."
             return 0
         fi
 
@@ -67,48 +81,71 @@ check_if_another_backup_is_running() {
         # Lets now get the Cronjob name because we'll need to determine who ran first at this point.
         # Is it the Cronjob or Job?
         # Get CronJob name
-        # CronJob names will always begin with "pingdirectory-periodic-backup"
-        active_cronjob_job_name=$(kubectl get jobs -o jsonpath='{.items[0].metadata.name}' -n "${NAMESPACE}" 2>/dev/null | grep 'pingdirectory-periodic-backup-' | tail -n 1)
+        # CronJob name will always begin with "pingdirectory-periodic-backup" follow by a timestamp in its name.
+        active_cronjob_job_name=$(get_actively_running_cronjob | head -n 1)
 
         # Now that we have both names (Cronjob and manual Job).
         # We can sort the create timestamp and determine who ran first (Cronjob or manual Job).
-        second_job_by_name=$(kubectl get job "${active_cronjob_job_name}" "${active_manual_job_name}" --sort-by=.metadata.creationTimestamp -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' -n "${NAMESPACE}" 2>/dev/null | sed -n '2p')
+        second_active_job_by_name=$(kubectl get jobs "${active_cronjob_job_name}" "${active_manual_job_name}" --sort-by=.metadata.creationTimestamp -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' -n "${NAMESPACE}" 2>/dev/null | sed -n '2p')
 
         # Avoid interrupting backup that started first. Delete the second backup only.
-        echo "There is a backup already running at the moment. Terminating ${second_job_by_name} Job"
+        echo "There is a backup already running at the moment. Terminating ${second_active_job_by_name} Job"
 
-        # Before deleting this Job pause so BeOps can see logs.
+        # Before deleting this Job that ran second pause so the person can see the logs.
         sleep 30
 
-        # Terminate 2nd Job
-        kubectl delete job "${second_job_by_name}" -n "${NAMESPACE}"
+        # Terminate second Job
+        kubectl delete job "${second_active_job_by_name}" -n "${NAMESPACE}"
 
     else
 
         # Entering this condition means there is NOT an active Cronjob running.
 
         # Determine if there is another Manual Job running. We also need to avoid 2 manual Jobs from running.
-        # Manual jobs can be found by filtering on "manual=true" label.
+        # Manual jobs can be found by filtering on "pd-manual=true" label.
         # We can sort the create timestamp and retrieve the 2nd manual Job if there is any.
-        second_manual_job_by_name=$(kubectl get job --selector=manual=true --sort-by=.metadata.creationTimestamp -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' -n "${NAMESPACE}" 2>/dev/null | sed -n '2p')
+        second_active_manual_job_by_name=$(get_actively_running_manual_jobs | sed -n '2p')
 
-        if [ -z "${second_manual_job_by_name}" ]; then
-            echo "Exiting because a second manual Job was not found"
+        if [ -z "${second_active_manual_job_by_name}" ]; then
+            echo "Second manual Job was not found. Ready to proceed."
             return 0
         fi
 
         # Avoid interrupting manual backup Job that started first. Delete the second backup only.
-        echo "There is a manual backup already running at the moment. Terminating ${second_manual_job_by_name} Job"
+        echo "There is a manual backup already running at the moment. Terminating ${second_active_manual_job_by_name} Job"
 
-        # Before deleting this Job pause so BeOps can see logs.
+        # Before deleting this Job that ran second pause so the person can see the logs.
         sleep 30
 
-        # Terminate 2nd Job
-        kubectl delete job "${second_manual_job_by_name}" -n "${NAMESPACE}"
+        # Terminate second Job
+        kubectl delete job "${second_active_manual_job_by_name}" -n "${NAMESPACE}"
     fi
 
     return 1 # Default, exit method as there is another backup running.
              # This should never happen if there is not collision with Cronjob and manual Job.
+}
+
+# Function ensures that the pd-manual=true label is attached to manual Jobs. This is the only way p1as automation
+# can filter out pingdirectory backups
+is_required_label_for_manual_job_provided() {
+
+    # Determine if Cronjob is actively running. This will be empty if CronJob is not running.
+    active_cronjob_running=$(is_cronjob_running_now)
+    if [ -z "${active_cronjob_running}" ]; then
+      # This is a manual Job. To get the Job name use the pod metadata.ownerReferences
+      current_manual_job_name=$(kubectl get pod "${CURRENT_JOB_POD_NAME}" -n "${NAMESPACE}" -o jsonpath='{.metadata.ownerReferences[0].name}')
+      # Verify that manual Job is labelled correctly as 'pd-manual=true'.
+      kubectl get job "${current_manual_job_name}" -n "${NAMESPACE}" -o jsonpath='{.metadata.labels.pd-manual}' | grep -q "true"
+      if [ $? -ne 0 ]; then
+        echo "Exiting. Manual Job must have the required label 'pd-manual=true'"
+        sleep 30
+        return 1
+      else
+        echo "Required label 'pd-manual=true' was found for job. Proceeding with backup."
+      fi
+    fi
+
+    return 0 # This is a CronJob or the correct label was found for manual Job
 }
 
 ### Script execution begins here. ###
@@ -116,11 +153,15 @@ check_if_another_backup_is_running() {
 # This guarantees that cleanup_resources method will always run, even if the script exits due to an error
 trap "cleanup_resources" EXIT
 
-if ! check_if_another_backup_is_running; then
-  # Prepare to exit because there is a CronJob/manual Job collision.
-  # Avoid cleaning up resources because the 1st Job actually is using the PVC.
+sleep 15 # Allow users who create manual Job to attach label
+
+# Before kicking off the PD backup. Check the following conditions:
+# 1) If there is a CronJob/manual Job collision.
+# 2) If user created manual Job but without the appropriate label.
+if ! is_only_one_backup_running || ! is_required_label_for_manual_job_provided; then
+  # Avoid automation from deleting PVC. We need to avoid deleting if this is a CronJob/manual Job collision.
   SKIP_RESOURCE_CLEANUP="true"
-  exit 0 # Exit graceful. Technically, this is not a backup error.
+  exit 0 # Gracefully exit. This is not a PingDirectory backup error but a user error.
 fi
 
 # Before backup begins. Ensure lingering resources of Job and PVC have been removed when running prior backup
@@ -132,48 +173,41 @@ test -x ${SCRIPT} && ${SCRIPT} "p1as-automation"
 # Wait for Job to be in 'Complete' state
 while true; do
 
-  # Verify the pod of the Job has been deployed before checking Job state.
-  POD_NAME=$(get_pod_name)
-  if [ -z "${POD_NAME}" ]; then
-    echo "Pod with prefix ${BACKUP_NAME} not found. Waiting for ${BACKUP_NAME} Job to deploy..."
+  # The pod of the Job is running. The following logic will now check for completion on the Job K8s object
+  if is_job_complete; then
+    echo "${BACKUP_NAME} Job successfully completed. Cronjob will clean up the backup job PVC and Job resources"
+    exit 0
   else
 
-    # The pod of the Job is running. The following logic will now check for completion on the Job K8s object
-    if is_job_complete; then
-      echo "${BACKUP_NAME} Job successfully completed. Cronjob will clean up the backup job PVC and Job resources"
-      exit 0
+    # Job is not complete yet once in this else condition.
+
+    # Now, check to ensure Job backofflimit/retries hasn't exceeded.
+    # If so, immediately terminate Cronjob with error as its retries of backup Job has exceeded.
+    # As of now we have backofflimit set to 0 in K8s Job 'pingdirectory-backup' so technically we shouldn't need this logic.
+    # However, if this were to ever change. The cronjob is smart enough to keep Job and PVC until it has exceeded
+    # its backofflimit/retry attempts of producing a backup.
+
+
+    # Retrieve failed attempts of Job if exist and collect backofflimit/# of retries from Job K8s spec
+    failed_attempts=$(get_failed_attempts)
+    backoff_limit=$(get_backoff_limit)
+
+    # If we can't find any failures assume Job is still running
+    if [ -z "${failed_attempts}" ]; then
+      echo "${BACKUP_NAME} Job is running but not complete. Waiting..."
+
+    # Failed attempts was found check to see if it exceeds backofflimit. If so, we can stop the cronjob and report
+    # as an error
+    elif [ "${failed_attempts}" -ge "${backoff_limit}" ]; then
+      echo "Job failed ${failed_attempts} times, with backofflimit of ${backoff_limit}. Job has exceeded its backofflimit/retries. Exiting with error..."
+      echo "Cronjob will clean up the backup job PVC and Job resources"
+      exit 1
+
+    # Failed attempts have not exceeded. This means K8s Job pingdirectory-backup backofflimit hasn't exceeded so
+    # The cronjob should be aware and avoid deleting the Job and PVC. Cronjob will continue to wait until backofflimit
+    # has exceeded or until a successful completion of backup.
     else
-
-      # Job is not complete yet once in this else condition.
-
-      # Now, check to ensure Job backofflimit/retries hasn't exceeded.
-      # If so, immediately terminate Cronjob with error as its retries of backup Job has exceeded.
-      # As of now we have backofflimit set to 0 in K8s Job 'pingdirectory-backup' so technically we shouldn't need this logic.
-      # However, if this were to ever change. The cronjob is smart enough to keep Job and PVC until it has exceeded
-      # its backofflimit/retry attempts of producing a backup.
-
-
-      # Retrieve failed attempts of Job if exist and collect backofflimit/# of retries from Job K8s spec
-      failed_attempts=$(get_failed_attempts)
-      backoff_limit=$(get_backoff_limit)
-
-      # If we can't find any failures assume Job is still running
-      if [ -z "${failed_attempts}" ]; then
-        echo "${BACKUP_NAME} Job is running but not complete. Waiting..."
-
-      # Failed attempts was found check to see if it exceeds backofflimit. If so, we can stop the cronjob and report
-      # as an error
-      elif [ "${failed_attempts}" -ge "${backoff_limit}" ]; then
-        echo "Job failed ${failed_attempts} times, with backofflimit of ${backoff_limit}. Job has exceeded its backofflimit/retries. Exiting with error..."
-        echo "Cronjob will clean up the backup job PVC and Job resources"
-        exit 1
-
-      # Failed attempts have not exceeded. This means K8s Job pingdirectory-backup backofflimit hasn't exceeded so
-      # The cronjob should be aware and avoid deleting the Job and PVC. Cronjob will continue to wait until backofflimit
-      # has exceeded or until a successful completion of backup.
-      else
-        echo "Job failed ${failed_attempts} times, with backofflimit of ${backoff_limit}. ${BACKUP_NAME} Job is expected to retry."
-      fi
+      echo "Job failed ${failed_attempts} times, with backofflimit of ${backoff_limit}. ${BACKUP_NAME} Job is expected to retry."
     fi
   fi
 
