6f43bfe620e21ed5e5676f416c6f817448a47a6a
Jack Miner <jackminer@pingidentity.com>
2024-10-18 13:21:06 -0600

    Update test to use compile stage instead

--- tests/ingress-nginx/03-test-manifest.sh
+++ tests/ingress-nginx/03-test-manifest.sh
@@ -8,44 +8,10 @@ if skipTest "${0}"; then
   exit 0
 fi
 
-THIS_TEST=$(basename "$0")
-UBER_YAML_OUTPUT="/tmp/${THIS_TEST}/test-uber-output.yaml"
-
-setUp() {
-  local this_test
-  this_test="$(basename "$0")"
-  local csr_path="/tmp/${this_test}/test-csr"
-  local csr_name=${CLUSTER_STATE_REPO_URL##*\/}
-
-  ## Special env setup for git-ops-command.sh to work properly in these tests ##
-  # Set LOCAL for to pull the PCB_PATH properly
-  export LOCAL="true"
-  # Use PROJECT_DIR if provided manually, otherwise use CI_PROJECT_DIR set by Gitlab,
-  # since PCB will already be checked out
-  export PCB_PATH=${PROJECT_DIR:-$CI_PROJECT_DIR}
-
-  # Remove CSR if it exists, moved from tearDown as pwd errors were occurring
-  rm -rf "${csr_path}"
-
-  # NOTE: copy of logic from k8s-deploy-tools/ci-scripts/k8s-deploy/deploy.sh
-  local branch_name=""
-  if [[ ${ENV_TYPE} == "prod" ]]; then
-    branch_name="master"
-  else
-    branch_name="${ENV_TYPE}"
-  fi
-
-  mkdir -p "${csr_path}"
-  cd "${csr_path}" || exit 1
-  git clone -b "${branch_name}" "codecommit://${csr_name}" .
-  cd k8s-configs || exit 1
-  log "Generating uber yaml, this may take some time..."
-  ./git-ops-command.sh "${REGION}" > "${UBER_YAML_OUTPUT}"
-}
-
 testNGINXVersionMatch() {
   export nginx_expected_version="1.11.2"
-  yq -e 'select(.metadata.labels."app.kubernetes.io/name" == "ingress-nginx") | select(.metadata.labels."app.kubernetes.io/version" != env(nginx_expected_version))' "${UBER_YAML_OUTPUT}"
+  # Use UBER_YAML outputted by compile stage to check version of ingress-nginx
+  yq -e 'select(.metadata.labels."app.kubernetes.io/name" == "ingress-nginx") | select(.metadata.labels."app.kubernetes.io/version" != env(nginx_expected_version))' "${UBER_YAML}"
   assertEquals "Some matches were found matching a version other than expected, see output from yq above^^" 1 $?
 }
 
