492f332ddf61517946a25160a960baa575906fe3
walkerbrown <walkerbrown@pingidentity.com>
2024-11-05 15:27:13 -0700

    inital commit , reworking test

--- tests/monitoring/test_fluent_health.py
+++ tests/monitoring/test_fluent_health.py
@@ -20,26 +20,21 @@ class PrometheusPortForward:
             ["kubectl", "port-forward", "svc/prometheus", "9090:9090", "-n", "prometheus"],
             stdout=subprocess.PIPE
         )
-        for _ in range(10):
-            if PrometheusPortForward.process.poll() is None:
-                time.sleep(10)
-                continue
-            else:
-                break
-        return PrometheusPortForward.process
+        # Wait for port-forward to start
+        time.sleep(3)
 
     @staticmethod
     def stop():
         if PrometheusPortForward.process:
             PrometheusPortForward.process.terminate()
-            PrometheusPortForward.process = None
 
 def query_metric(metric_name, prometheus_url):
     try:
         response = requests.get(f"{prometheus_url}?query={metric_name}", verify=False)
         if response.status_code == 200:
             result = response.json()['data']['result']
-            return float(result[0]['value'][1]) if result else None
+            total = sum(float(item['value'][1]) for item in result)
+            return total
     except requests.exceptions.ConnectionError as e:
         print(f"Error querying Prometheus: {e}")
     return None
@@ -65,35 +60,32 @@ class TestFluentBitMetrics(unittest.TestCase):
             print(f"Pod {pod_name} is ready")
         print("All Fluent Bit pods are up and running.")
 
-    def test_fluentbit_pods_health(self):
-        self.check_all_fluentbit_pods_ready()
-
     def test_check_fluentbit_metrics(self):
         self.check_all_fluentbit_pods_ready()
         PrometheusPortForward.start()
+        try:
+            attempt = 0
+            max_attempts = 10
+            while attempt < max_attempts:
+                input_records = query_metric("fluentbit_input_records_total", self.prometheus_url)
+                output_records = query_metric("fluentbit_output_proc_records_total", self.prometheus_url)
+                print(output_records)
+                if input_records is None or output_records is None:
+                    print("Metrics not found or connection issue. Restarting port-forward and retrying...")
+                    PrometheusPortForward.start()
+                elif input_records > 0 and output_records > 0:
+                    print(f"Attempt {attempt+1}: Metrics found.")
+                    print(f"fluentbit_input_records_total: {input_records}")
+                    print(f"fluentbit_output_proc_records_total: {output_records}")
+                    break
+                else:
+                    print(f"Attempt {attempt+1}: Metrics issue: input={input_records}, output={output_records}")
 
-        attempt = 0
-        max_attempts = 10
-        while attempt < max_attempts:
-            input_records = query_metric("fluentbit_input_records_total", self.prometheus_url)
-            output_records = query_metric("fluentbit_output_proc_records_total", self.prometheus_url)
-
-            if input_records is None or output_records is None:
-                print("Metrics not found or connection issue. Restarting port-forward and retrying...")
-                PrometheusPortForward.start()
-            elif input_records > 0 and output_records > 0:
-                print(f"Attempt {attempt+1}: Metrics found.")
-                print(f"fluentbit_input_records_total: {input_records}")
-                print(f"fluentbit_output_proc_records_total: {output_records}")
-                break
-            else:
-                print(f"Attempt {attempt+1}: Metrics issue: input={input_records}, output={output_records}")
-
-            attempt += 1
-            time.sleep(10)
-
+                attempt += 1
+                time.sleep(10)
+        finally:
+            PrometheusPortForward.stop()
         self.assertLess(attempt, max_attempts, "Metrics check failed after max attempts.")
-        PrometheusPortForward.stop()
 
 if __name__ == '__main__':
     unittest.main()
