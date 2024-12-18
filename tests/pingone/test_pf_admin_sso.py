113f3bc614ec5c50aa2db86185317ff626aee07d
Colin Reininger <colinreininger@pingidentity.com>
2024-12-18 16:38:42 -0700

    share code across operator tests

--- tests/pingone/test_pf_admin_sso.py
+++ tests/pingone/test_pf_admin_sso.py
@@ -69,32 +69,12 @@ class TestPFAdminSSO(p1_test_base.P1TestBase):
         self.assertTrue(resource, f"Resource '{self.operator_resource_name}' not created")
 
     def test_operator_scope_granted_to_app(self):
-        app = self.get(self.cluster_env_endpoints.applications, self.app_name)
-        grants = self.get(
-            f"{self.cluster_env_endpoints.applications}/{app['id']}/grants"
-        )
-        # Get granted resource and scope IDs
-        granted_resource_ids = []
-        granted_scope_ids = []
-        for grant in grants:
-            granted_resource_ids.append(grant["resource"]["id"])
-            granted_scope_ids += [scope["id"] for scope in grant["scopes"]]
+        app_scope_ids = self.get_app_scope_ids(self.app_name)
+        operator_scope_id = self.get_resource_scope_id(self.operator_resource_name, self.operator_scope_name)
 
-        # Get account_type scope IDs from granted resources
-        operator_scope_ids = []
-        for resource_id in granted_resource_ids:
-            scope = self.get(
-                endpoint=f"{self.cluster_env_endpoints.resources}/{resource_id}/scopes",
-                name=self.operator_scope_name,
-            )
-            operator_scope_ids.append(scope["id"])
-
-        # Check that one of the granted scope IDs is an account_type scope
+        # Check that one of the granted scope IDs is the operator scope
         self.assertTrue(
-            any(
-                granted_scope_id in operator_scope_ids
-                for granted_scope_id in granted_scope_ids
-            ),
+            operator_scope_id in app_scope_ids,
             f"No grant for scope '{self.operator_scope_name}' found for application '{self.app_name}'",
         )
 
