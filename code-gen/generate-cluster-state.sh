7b19fd7e3cc5091a4847437e72c761843cdb82c1
abhalla <abhalla@pingidentity.com>
2026-02-16 14:57:37 +0530

    ebs kms changes

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -1390,7 +1390,7 @@ for ENV_OR_BRANCH in ${SUPPORTED_ENVIRONMENT_TYPES}; do
   #Getting Global Domain
   set_var "GLOBAL_TENANT_DOMAIN" "global.${TENANT_DOMAIN_NO_DOT_SUFFIX}" "/pcpt/global-dns/hosted-zone/zone-name" ""
 
-  set_var "EBS_KMS_KEY_ARN" "" "${ACCOUNT_BASE_PATH}" "${ENV}/customer/ebs/kms/key/arn
+  set_var "EBS_KMS_KEY_ARN" "" "${ACCOUNT_BASE_PATH}" "${ENV}/customer/ebs/kms/key/arn" ""
 
 
   ######################################################################################################################
